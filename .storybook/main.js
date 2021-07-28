230f94c3bbc5984d9a97ef5a4a5917fa1220fb43
Hiroaki Sasaki <sasa.hiro0125@gmail.com>
2021-07-28 16:05:09 +0900

    fix storybook settings

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
-    stories: [
-        "../src/**/*.stories.mdx",
-        "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    ],
+    features: {
+        previewCsfV3: true,
+    },
+    stories: ["../src"],
     addons: [
         "@storybook/addon-links",
         "@storybook/addon-essentials",
